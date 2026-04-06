ca7201552ceb246cd2c9758b3565e69a9574a96b
Ojus Tripathi <tripathiojuss@gmail.com>
2026-04-07 01:47:47 +0530

    Add files via upload

---
+++ Fizz_buzz.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+vector<string> fizzBuzz(int n) {
+    vector<string> answer;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            answer.push_back("FizzBuzz");
+        } else if (i % 3 == 0) {
+            answer.push_back("Fizz");
+        } else if (i % 5 == 0) {
+            answer.push_back("Buzz");
+        } else {
+            answer.push_back(to_string(i));
+        }
+    }
+
+    return answer;
+}
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    vector<string> result = fizzBuzz(n);
+
+    for (const string &s : result) {
+        cout << s << " ";
+    }
+
+    return 0;
+}
